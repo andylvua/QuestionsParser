a2f9a0dd411ecb011edbfab4537c4f18cc019c04
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-09-28 20:31:15 +0300

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     setup(
         name='QuestionsParser',
         packages=find_packages(),
-        version='0.0.5',
+        version='1.0.0',
         description='A Python package for parsing questions from pdf files.',
         author='Andrew Yaroshevych',
         install_requires=[
